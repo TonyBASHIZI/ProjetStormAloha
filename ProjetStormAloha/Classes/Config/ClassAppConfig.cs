5c02796c5213d0609c7962142af402c7665f544c
Ardin Biganiro <ardinbig@gmail.com>
2019-10-21 10:47:22 +0200

    Add default config

---
+++ ProjetStormAloha/Classes/Config/ClassAppConfig.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Security.Permissions;
+
+namespace ProjetStormAloha.Classes.Config
+{
+    public class AppConfig
+    {
+        public static string InitialDirectory
+        {
+            get
+            {
+                return @"C:\ProgramData\Aloha Dynamics\Storm sport\";
+            }
+        }
+
+        public static string BackupDirectory
+        {
+            get
+            {
+                return InitialDirectory + @"\Backup\";
+            }
+        }
+
+        public static string ConnectionDirectory
+        {
+            get
+            {
+                return InitialDirectory + @"\Connection\";
+            }
+        }
+
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConnectionDirectory + "path.txt";
+            }
+        }
+
+        public static string GetConnectionString
+        {
+            get
+            {
+                return File.ReadAllText(ConnectionString).Trim();
+            }
+        }
+
+        public static void CreateInitialDirectory()
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(InitialDirectory);
+                if (!Directory.Exists(InitialDirectory))
+                {
+                    Directory.CreateDirectory(InitialDirectory);
+                    dirInfo.Attributes = FileAttributes.Hidden;
+                }
+                dirInfo.Attributes = FileAttributes.Hidden;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Une erreur s'est produite. \n" + ex.Message);
+            }
+        }
+
+        public static void CreateConnectionDirectory()
+        {
+            try
+            {
+                if (!Directory.Exists(ConnectionDirectory))
+                {
+                    Directory.CreateDirectory(ConnectionDirectory);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Une erreur s'est produite. \n" + ex.Message);
+            }
+        }
+
+        public static void CreateConnectionString()
+        {
+            try
+            {               
+                if (!File.Exists(ConnectionString))
+                {
+                    File.Create(ConnectionString);
+                    FileIOPermission file = new FileIOPermission(FileIOPermissionAccess.Read | FileIOPermissionAccess.Write, ConnectionString);
+                    file.AddPathList(FileIOPermissionAccess.Read | FileIOPermissionAccess.Write, ConnectionString);
+                    file.Demand();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Une erreur s'est produite. \n" + ex.Message);
+            }
+        }
+
+        public static bool InitialDirectoryExist()
+        {
+            return Directory.Exists(InitialDirectory) ? true : false;
+        }
+
+        public static bool ConnectionStringExist()
+        {
+            return File.Exists(ConnectionString) ? true : false;
+        }
+
+        public static bool ConnectionStringEmpty()
+        {
+            return GetConnectionString.Length == 0 ? true : false;
+        }
+    }
+}
---
+++ ProjetStormAloha/Classes/Config/ClassConstant.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjetStormAloha.Classes.Config
+{
+    public abstract class Constant
+    {
+        public abstract class Database
+        {
+            public static string InitialDirectory = AppConfig.InitialDirectory;
+            public static string Path = AppConfig.GetConnectionString;
+            public static string Backup = AppConfig.BackupDirectory;
+        }
+
+        public abstract class Table
+        {
+            public static string Client = "client";
+            public static string Carte = "carte";
+            public static string Compte = "compte";
+            public static string Agent = "agent";
+            public static string Pos = "pos";
+        }
+    }
+}
