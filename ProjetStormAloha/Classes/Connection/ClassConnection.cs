c17675088e76d9ca3f38b9b73623b73065af1f69
Ardin Biganiro <ardinbig@gmail.com>
2019-10-21 10:49:56 +0200

    Add connection classe

---
+++ ProjetStormAloha/Classes/Connection/ClassConnection.cs
@@ -0,0 +1,29 @@
+﻿using ProjetStormAloha.Classes.Config;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ProjetStormAloha.Classes.Connection
+{
+    public class Connection
+    {
+        private static Connection _instance = null;
+
+        public IDbConnection Con { get; set;}
+
+        public static Connection Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Connection();
+                return _instance;
+            }
+        }
+
+        public IDbConnection Connect()
+        {
+            Con = new MySqlConnection(Constant.Database.Path);
+            return Con;
+        }        
+    }
+}
